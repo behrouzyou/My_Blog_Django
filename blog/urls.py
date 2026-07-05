15950153d205570a1beacac41e74d5446f716b0b
behrouzyou <behrouzyou@gmail.com>
2026-07-05 17:46:20 +0330

    [200~build templates static(index,base,profile,article,..)

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('article/sample-slug/',views.article_detail,name='article-detail'),
+    path('login/',views.login,name='login'),
+    path('signup/',views.signup,name='signup'),
+    path('profile/',views.profile,name='profile'),
+    path('article/create/',views.article_form,name='article-create')
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request,'index.html')
+def article_detail(request):
+    return render(request,'article_detail.html')
+def login(request):
+    return render(request,'login.html')
+def signup(request):
+    return render(request,'signup.html')
+def profile(request):
+    return render(request,'profile.html')
+def article_form(request):
+    return render(request,'article_form.html')
